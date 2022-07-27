cf0663bf4ffe50616c738a0b2fe3bf51acfd5663
Paul McAllister <360455+pj530i@users.noreply.github.com>
2022-07-26 23:10:55 -0400

    Exercises 4-7 complete.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    sum = 0;
+    let numStart = 0;
+    let numEnd = 0;
+
+    if( num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2))
+    {
+        return 'ERROR';
+    } else if ( num1 > num2) {
+        numStart = num2;
+        numEnd = num1;
+    } else {
+        numStart = num1;
+        numEnd = num2;
+    }
+
+    for( let i = numStart; i <= numEnd; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
