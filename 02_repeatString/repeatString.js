33f17d57668cbea0f6701dccb7ca9e0a32ab0e05
Paul McAllister <360455+pj530i@users.noreply.github.com>
2022-07-26 21:38:46 -0400

    Exercises 1-3 complete.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function( str, repeats) {
+    if(repeats < 0) {
+        return 'ERROR'
+    } else {
+        return str.repeat(repeats);
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let revStr = str;
+    return revStr = revStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
