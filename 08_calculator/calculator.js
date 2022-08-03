0cba97cb728c3747e8257af7bd2148ca0281bf2a
Paul McAllister <360455+pj530i@users.noreply.github.com>
2022-08-03 00:14:59 -0400

    Completed 04 Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(ary) {
+  // sumvVal contains running total, 0 arg inits it
+  return ary.reduce( (sumVal, val) => sumVal + val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(ary) {
+  return ary.reduce( (multVal, val) => multVal * val, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  let cur = a;
+  
+
+  while(cur - 1 > 0) {
+    fact *= cur--;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
