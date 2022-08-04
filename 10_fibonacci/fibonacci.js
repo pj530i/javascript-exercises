5290cb87efc67d427ce6e5b088f9e37d02a3c2d6
Paul McAllister <360455+pj530i@users.noreply.github.com>
2022-08-03 23:28:17 -0400

    Completed fibonacci exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const converted = parseInt(num);
+    if(!converted || converted < 1) {
+        return 'OOPS';
+    }
 
+    let fib = 0;
+    let last1 = 0;
+    let last2 = 1;
+    let pos = 0;
+    
+    do {
+        fib = last1 + last2;
+        last1 = last2;
+        last2 = fib;
+
+    } while( ++pos < converted - 1)
+
+    return fib;
 };
 
 // Do not edit below this line
