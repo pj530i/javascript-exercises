8036d966af6b8eacc5bc689406e3fe93e4b48a6a
Paul McAllister <360455+pj530i@users.noreply.github.com>
2022-08-03 22:59:53 -0400

    Palindromes exercise complete.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let strArray = Array.from(str.toUpperCase().split(''));
+    let filtered = strArray.filter( char => char.toUpperCase() != char.toLowerCase() );
 
-};
+    return filtered.every((char, index) => {
+        if(index < Math.floor(filtered.length / 2 )) {
+            return char == filtered[filtered.length - 1 - index];
+        } else {
+            return true;
+        }
+    })
+}
 
 // Do not edit below this line
 module.exports = palindromes;
