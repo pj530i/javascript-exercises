e3971a3ba148e1f9b1ccd25e494b68f5a1b473c0
Paul McAllister <360455+pj530i@users.noreply.github.com>
2022-08-04 00:03:01 -0400

    Completed find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const getCurrentAge = function(person) {
+    // If still alive use current year to determine ago.  If birth year isn't defined it's prolly the default empty object
+    const lastYear = ('yearOfDeath' in person) ? person['yearOfDeath'] : (new Date()).getFullYear();
+    const birthYear = ('yearOfBirth' in person) ? person['yearOfBirth'] : (new Date()).getFullYear();
+    return lastYear - birthYear;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce( 
+        (oldest, person) => getCurrentAge(person) > getCurrentAge(oldest) ? person : oldest, {} );
 };
 
 // Do not edit below this line
